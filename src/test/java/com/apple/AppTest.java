290d71abab854acc599c142ddedcdccb6e1f8aa6
陈璐 <lu.chen@htouhui.com>
2019-10-08 11:18:41 +0800

    修改测试

--- src/test/java/com/apple/AppTest.java
+++ src/test/java/com/apple/AppTest.java
@@ -1,11 +1,5 @@
 package com.apple;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
-import java.util.regex.Pattern;
-
 /**
  * Unit test for simple App.
  */
@@ -13,24 +7,11 @@ public class AppTest {
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
-
-
     public static void main(String[] args) {
         String str = "100积分肝功能";
         System.out.println(str.indexOf("积"));
 //        System.out.println(str.substring(0,str.indexOf("积")));
 
         System.out.println(str.replace("[^0-9]",""));
-
-
-
     }
-
-
-
-
 }
