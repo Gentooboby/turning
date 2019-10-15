62f92e24f37347731d076cf4288871f333b0f27e
陈璐 <lu.chen@htouhui.com>
2019-10-15 14:37:40 +0800

    枚举

--- src/main/java/com/apple/thinking/interfaces/Adventure.java
+++ src/main/java/com/apple/thinking/interfaces/Adventure.java
@@ -5,6 +5,29 @@ package com.apple.thinking.interfaces;
  * @version 1.0
  */
 public class Adventure {
+    public static void  t(CanFight x){
+        x.fight();
+    }
+
+    public static void u(CanSwim x){
+        x.swim();
+    }
+
+    public static void v(CanFly x){
+        x.fly();
+    }
+
+    public static void w(ActionCharacter x){
+        x.fight();
+    }
+
+    public static void main(String[] args) {
+        Hero h = new Hero();
+        t(h);
+        u(h);
+        v(h);
+        w(h);
+    }
 }
 
 interface CanFight {
